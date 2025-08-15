a1198619a5d50c791c427dc3c301194aad233b7e
ecy <ecyking72345@gmail.com>
2025-08-15 01:44:27 -0700

    final changes

--- components/buttons/ButtonTemplate.js
+++ components/buttons/ButtonTemplate.js
@@ -13,3 +13,17 @@ export default function ButtonTemplate({className, onClick, icon, label, color="
     );
 
 }
+export function ModButtonTemplate({mod,className, onClick, icon, label, color="white"}) {
+    // two classes? $`className`
+    if (!mod) return
+    return (
+        <div 
+            className={'mod-button' } 
+            style={{backgroundColor: color, color: 'white'}} 
+            onClick={onClick}>
+            <FontAwesomeIcon icon={icon} />
+            <span>{label}</span> 
+        </div>
+    );
+
+}
--- components/modals/moreModal.js
+++ components/modals/moreModal.js
@@ -10,7 +10,7 @@ import ThreeButtons, { DeleteButton, ModButton } from '../buttons/ThreeButtons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { isAdmin } from '@/utils/modFns';
 import { useSession } from 'next-auth/react';
-import ButtonTemplate from '../buttons/ButtonTemplate';
+import ButtonTemplate, { ModButtonTemplate } from '../buttons/ButtonTemplate';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -41,18 +41,18 @@ export default function ModalView({card, show, setShow, countryCode, mod, setRef
 
 
     function MoreModalFooter() {
+        // on click favorites in db going to Ecy land
     return (
         <Modal.Footer>
-         <div>
-             <ButtonTemplate 
+             <ModButtonTemplate 
              onClick={() => console.log("sga")} 
              icon={faStar} 
              label="favorite"
              color="gold"
+             mod={admin}
              />
             {/* can delete means either is admin or card author matches current user */}
             <ExtraDelete/>
-        </div>
 
 {/* could potentiall put {card.name} but rn that's email */}
         <div> made by  </div>
