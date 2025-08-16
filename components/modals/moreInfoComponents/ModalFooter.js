05da1e135d17d9437c85e3e1ae34dac7c24217f4
ecy <ecyking72345@gmail.com>
2025-08-15 17:12:49 -0700

    slight fix

--- components/modals/moreInfoComponents/ModalFooter.js
+++ components/modals/moreInfoComponents/ModalFooter.js
@@ -8,14 +8,41 @@ import { CurrentUserImage, UserImage } from '@/components/buttons/icons';
 function ExtraDelete({ card, setShow, admin, onModPage }) {
   if (onModPage) return null; // Do not show on the mod page
   if (!admin) return null; // Not possible unless admin
-
   return <DeleteButton card={card} setShow={setShow} />;
 }
 
 export default function ModalFooter({ card, setShow, admin, onModPage, currentUser, name, image, onClick }) {
   return (
-    <Modal.Footer>
-      <span>
+        <Modal.Footer>
+             <ModButtonTemplate 
+                onClick={() => console.log("sga")} 
+                icon={faStar} 
+                label="favorite"
+                color="gold"
+                mod={admin}
+             />
+            {/* can delete means either is admin or card author matches current user */}
+              <ExtraDelete card={card} setShow={setShow} admin={admin} onModPage={onModPage} />
+
+
+          {/* could potentiall put {card.name} but rn that's email */}
+          <div> made by: </div>
+          <div >
+                  {currentUser && <CurrentUserImage/>}
+                  {!currentUser && <UserImage name={name} image={image} onClick={onClick}/>}
+          </div>
+        
+          <Button variant="primary" onClick={() => setShow(close)}>
+              Close
+          </Button> 
+        </Modal.Footer>
+
+
+  );
+}
+
+
+      /* <span>
         
         <ModButtonTemplate onClick={() => console.log('favorite clicked')} icon={faStar} label="Favorite" color="gold" mod={admin} />
         <ExtraDelete card={card} setShow={setShow} admin={admin} onModPage={onModPage} />
@@ -26,8 +53,4 @@ export default function ModalFooter({ card, setShow, admin, onModPage, currentUs
         <Button variant="primary" onClick={() => setShow(false)}>
           Close
         </Button>
-      </span>
-
-    </Modal.Footer>
-  );
-}
+      </span> */
