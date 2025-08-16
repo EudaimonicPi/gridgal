e26a4777350996b172fd7bcf15bcf8471e42a026
ecy <ecyking72345@gmail.com>
2025-08-15 19:28:51 -0700

    says its good to go

--- app/page.js
+++ app/page.js
@@ -2,14 +2,16 @@
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { usePaginatedCards } from '@/hooks/usePaginatedCards';
-import HomepageHeader from '@/components/pageSections/headers/HomepageHeader'; // Import the new component
+// the culprit
+
+// import HomepageHeader from '@/components/pageSections/headers/HomepageHeader'; // Import the new component
 import { LoadingPage } from '@/components/elements/LoadingPage';
 import { getCards } from '@/utils/cards';
 import PaginationControls from '@/components/elements/PaginationPageControl';
 import '@/styles/cards.css';
 
 
-export default function Home() {
+export default function Page() {
   const { data, status } = useSession();
   const { cards, page, setPage, totalPages, isLoading } = usePaginatedCards(3); // 10 per page
   const [show, setShow] = useState(false);
@@ -20,17 +22,17 @@ export default function Home() {
   return (
     <main>
       {/* Use HomepageHeader component */}
-      <HomepageHeader
+      {/* <HomepageHeader
         show={show}
         setShow={setShow}
         confirmModalShow={confirmModalShow}
         setConfirmModalShow={setConfirmModalShow}
         isAuthenticated={status === 'authenticated'}
-      />
+      /> */}
 
-      {/* <div className="cards-container">
+      <div className="cards-container">
         {getCards(cards)}
-      </div> */}
+      </div>
 
       {/* Pagination Controls */}
       <PaginationControls page={page} totalPages={totalPages} setPage={setPage} />
