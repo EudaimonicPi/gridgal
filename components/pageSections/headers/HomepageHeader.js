2a961a0b29cd753a66ca5840850b3acfe9fece0e
ecy <ecyking72345@gmail.com>
2025-08-15 19:32:42 -0700

    everything but create modal

--- components/pageSections/headers/HomepageHeader.js
+++ components/pageSections/headers/HomepageHeader.js
@@ -1,7 +1,7 @@
 'use client';
 import CreateButton from '@/components/buttons/createButton';
 // import CreateModal from '@/components/modals/CreateCardModal';
-// import ConfirmModal from '@/components/modals/SubmittedGridModal';
+import ConfirmModal from '@/components/modals/SubmittedGridModal';
 import SignInButton from '@/components/buttons/signIn';
 import ProfilePic from '@/components/buttons/profilePic';
 
@@ -16,11 +16,11 @@ export default function HomepageHeader({ show, setShow, confirmModalShow, setCon
 
       <ProfilePic />
 
-      {/* <ConfirmModal
+      <ConfirmModal
         show={confirmModalShow}
         handleClose={() => setConfirmModalShow(false)}
       />
-      <CreateModal
+      {/* <CreateModal
         show={show}
         handleClose={() => setShow(false)}
         cards={[]}
